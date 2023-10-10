5f35e55df7bc3b9454078993c85219398baebd27
Nadya Voynich <nadya.voynich@gmail.com>
2023-10-10 17:43:42 +0200

    Created tables

--- sql_queries.py
+++ sql_queries.py
@@ -7,44 +7,134 @@ config.read('dwh.cfg')
 
 # DROP TABLES
 
-staging_events_table_drop = ""
-staging_songs_table_drop = ""
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events;"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs;"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
 
 # CREATE TABLES
 
-staging_events_table_create= ("""
+staging_events_table_create = ("""
+CREATE TABLE IF NOT EXISTS staging_events (
+    artist          varchar,
+    auth            varchar,
+    firstName       varchar,
+    gender          varchar,
+    iteminSession   smallint,
+    lastName        varchar,
+    length          float4,
+    level           varchar,
+    location        varchar,
+    method          varchar,
+    page            varchar,
+    registration    float8,
+    sessionId       integer,
+    song            varchar,
+    status          smallint,
+    ts              bigint,
+    userAgent       varchar,
+    userId          integer
+);
 """)
 
 staging_songs_table_create = ("""
+CREATE TABLE IF NOT EXISTS staging_songs (
+    num_songs           integer,
+    artist_id           varchar,
+    artist_latitude     float4,
+    artist_longitude    float4,
+    artist_location     varchar,
+    artist_name         varchar,
+    song_id             varchar,
+    title               varchar,
+    duration            float4,
+    year                smallint
+);
 """)
 
 songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplays (
+    songplay_id     bigint PRIMARY KEY,
+    start_time      timestamp NOT NULL,          
+    user_id         integer,         
+    level           varchar,
+    song_id         varchar,
+    artist_id       varchar,
+    session_id      integer,
+    location        varchar,
+    user_agent      varchar
+);
 """)
 
 user_table_create = ("""
+CREATE TABLE IF NOT EXISTS users (
+    user_id         integer PRIMARY KEY,
+    first_name      varchar,
+    last_name       varchar,
+    gender          varchar,
+    level           varchar,
+);
 """)
 
 song_table_create = ("""
+CREATE TABLE IF NOT EXISTS songs (
+    song_id         varchar PRIMARY KEY,
+    title           varchar,
+    artist_id       varchar,
+    year            smallint,
+    duration        float4,
+);
 """)
 
 artist_table_create = ("""
+CREATE TABLE IF NOT EXISTS artists (
+    artist_id       varchar PRIMARY KEY,
+    name            varchar,
+    location        varchar,
+    latitude        float4,
+    longitude       float4
+);
 """)
 
 time_table_create = ("""
+CREATE TABLE IF NOT EXISTS time (
+    start_time      timestamp PRIMARY KEY,
+    hour            smallint NOT NULL,            
+    day             smallint NOT NULL,
+    week            smallint NOT NULL,
+    month           smallint NOT NULL,
+    year            smallint NOT NULL,
+    weekday         smallint NOT NULL
+);
 """)
 
 # STAGING TABLES
 
 staging_events_copy = ("""
-""").format()
+    COPY staging_events
+    FROM {}
+    IAM_ROLE {}
+    JSON {}
+    REGION {};
+""").format(config['S3']['LOG_DATA'],
+            config['IAM_ROLE']['ARN'],
+            config['S3']['LOG_JSONPATH'],
+            config['CLUSTER']['REGION']
+            )
 
 staging_songs_copy = ("""
-""").format()
+    COPY staging_songs
+    FROM {}
+    IAM_ROLE {}
+    FORMAT AS JSON 'auto'
+    REGION {};
+""").format(config['S3']['LOG_DATA'],
+            config['IAM_ROLE']['ARN'],
+            config['CLUSTER']['REGION']
+            )
 
 # FINAL TABLES
 
